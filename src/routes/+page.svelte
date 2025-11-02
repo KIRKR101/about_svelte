20ebd4fd3e75f75fc4290a5e0ecc98fce3f1a8a7
theo <theokirk08@proton.me>
2025-11-02 18:26:13 +0000

    bun.lock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 
   onMount(async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/lastfm-track');
+      const response = await fetch('http://lastfm.kirkr.xyz/api/lastfm-track');
 
       if (!response.ok) {
         throw new Error(`API responded with ${response.status}`);
